ddb1650a49d6c30e0e0249fb37f614c0ff7619b0
Jonathan Goff <jrgoff@cascadetracking.org>
2014-08-05 16:14:44 -0700

    Split into functions to run on test data and to cross_validate just using the training data.
    Submitted generated test data to kaggle and scored ~.76.  I suspect that
    the reason for the earlier score of 0.0000 was formatting problem (had a
    space inbetween the columns in the csv).

--- kaggle.py
+++ kaggle.py
@@ -3,30 +3,61 @@ from numpy import genfromtxt, savetxt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 
-def run():
-    #Adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
-    #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
-    dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
-    
-    data = [x[1:-1] for x in dataset]
-    target = [x[-1] for x in dataset]
-    train_x, test_x, train_y, test_y =  cross_validation.train_test_split(data, target, test_size = 0.3)
-    #target = [x[-1] for x in dataset]
-    #train = [x[0:-1] for x in dataset]
-    #test = genfromtxt(open('train.csv','r'), delimiter=',', dtype='f8')[0:-1]
-    rf = RandomForestClassifier(n_estimators=100)
-    rf.fit(train_x, train_y)
-    #rf.fit(train[1:-1], train[-1])
-    return rf.score(test_x, test_y) 
-    #predicted = rf.predict(test) 
-    #indices = [x[0] for x in test]
-    #print predicted.shape()
-    #predicted_probs = [[index + 1, x[1]] for index, x in enumerate(rf.predict_proba(test))]
+def cross_validate():
+  #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
+  #seems to be working with this data: result ~.86
+  dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  
+  data = [x[1:-1] for x in dataset]
+  target = [x[-1] for x in dataset]
+  train_x, test_x, train_y, test_y =  cross_validation.train_test_split(data, target, test_size = 0.3)
+  rf = RandomForestClassifier(n_estimators=100)
+  rf.fit(train_x, train_y)
+  return rf.score(test_x, test_y) 
 
-    #output = np.array(zip(indices, predicted), dtype=[('id', int), ('ct',int)])
-    #savetxt('fake_submission.csv', output, delimiter=',', fmt='%d,%d', 
-            #header='Id,Cover_Type', comments = '')
-    #return output
+def test_data():
+  #Adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
+  dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  target = [x[-1] for x in dataset]
+  train = [x[1:-1] for x in dataset]
+  #print len(train[0])
+  #print target
+  test_dataset = genfromtxt(open('test.csv','r'), delimiter=',', dtype='f8')[1:]
+  test = [x[1:] for x in test_dataset]
+  indices = [x[0] for x in test_dataset]
+
+  rf = RandomForestClassifier(n_estimators=100)
+  rf.fit(train, target)
+  predicted = rf.predict(test) 
+  output = np.array(zip(indices, predicted), dtype=[('id', int), ('ct',int)])
+  savetxt('submission.csv', output, delimiter=',', fmt='%d,%d', 
+          header='Id,Cover_Type', comments = '')
+  return output
+
+def run():
+  #Adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
+  #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
+  dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  
+  data = [x[1:-1] for x in dataset]
+  target = [x[-1] for x in dataset]
+  train_x, test_x, train_y, test_y =  cross_validation.train_test_split(data, target, test_size = 0.3)
+  #target = [x[-1] for x in dataset]
+  #train = [x[0:-1] for x in dataset]
+  #test = genfromtxt(open('train.csv','r'), delimiter=',', dtype='f8')[0:-1]
+  rf = RandomForestClassifier(n_estimators=100)
+  rf.fit(train_x, train_y)
+  #rf.fit(train[1:-1], train[-1])
+  return rf.score(test_x, test_y) 
+  #predicted = rf.predict(test) 
+  #indices = [x[0] for x in test]
+  #print predicted.shape()
+  #predicted_probs = [[index + 1, x[1]] for index, x in enumerate(rf.predict_proba(test))]
+
+  #output = np.array(zip(indices, predicted), dtype=[('id', int), ('ct',int)])
+  #savetxt('fake_submission.csv', output, delimiter=',', fmt='%d,%d', 
+          #header='Id,Cover_Type', comments = '')
+  #return output
 
 if __name__=="__main__":
   run()
