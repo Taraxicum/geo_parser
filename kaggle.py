a4b54426a7758217d56ec0d53a978d51ec19faa4
Jonathan Goff <jrgoff@cascadetracking.org>
2014-08-06 00:12:42 -0700

    PCA - plotted 3 dimensional principle components of training data (adjusted a bit since then so might take a little fiddling to get it plotted again)
    pca_explore takes number of dimensions and returns X, y (training set,
    target set) nparrays with training set projected to dimension of the PCA
    
    Adjusted functions a bit to make loading data and transferring data from
    function to function more convenient.
    
    Example:
    X, y = kaggle.pca_explore(3)
    kaggle.rf_cross_validate(X=X, y=y, n=100)
    
    This does the 3-dim pca and returns the percent of correct
    classifications done using the reduced data and random forest.  I did
    several iterations of this and stored some of the results in google
    drive.

--- kaggle.py
+++ kaggle.py
@@ -1,21 +1,37 @@
 import numpy as np
 from numpy import genfromtxt, savetxt
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
+from sklearn import decomposition
 
-def cross_validate():
-  #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
-  #seems to be working with this data: result ~.86
+
+def load_training_data():
+  #loads training data returns as data, target
   dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  data = np.asarray([x[1:-1] for x in dataset])
+  target = np.asarray([x[-1] for x in dataset])
+  return data, target
+
+
+def rf_cross_validate(n=100, X=None, y=None):
+  #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
+  #Random Forest adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
+  if X==None or y == None:
+    X, y = load_training_data()
+  #dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
   
-  data = [x[1:-1] for x in dataset]
-  target = [x[-1] for x in dataset]
-  train_x, test_x, train_y, test_y =  cross_validation.train_test_split(data, target, test_size = 0.3)
-  rf = RandomForestClassifier(n_estimators=100)
+  #data = [x[1:-1] for x in dataset]
+  #target = [x[-1] for x in dataset]
+  train_x, test_x, train_y, test_y =  cross_validation.train_test_split(X, y, test_size = 0.3)
+  rf = RandomForestClassifier(n)
   rf.fit(train_x, train_y)
   return rf.score(test_x, test_y) 
 
-def test_data():
+def rf_test_data():
   #Adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
   dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
   target = [x[-1] for x in dataset]
@@ -34,30 +50,55 @@ def test_data():
           header='Id,Cover_Type', comments = '')
   return output
 
-def run():
-  #Adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
-  #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
-  dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
-  
-  data = [x[1:-1] for x in dataset]
-  target = [x[-1] for x in dataset]
-  train_x, test_x, train_y, test_y =  cross_validation.train_test_split(data, target, test_size = 0.3)
-  #target = [x[-1] for x in dataset]
-  #train = [x[0:-1] for x in dataset]
-  #test = genfromtxt(open('train.csv','r'), delimiter=',', dtype='f8')[0:-1]
-  rf = RandomForestClassifier(n_estimators=100)
-  rf.fit(train_x, train_y)
-  #rf.fit(train[1:-1], train[-1])
-  return rf.score(test_x, test_y) 
-  #predicted = rf.predict(test) 
-  #indices = [x[0] for x in test]
-  #print predicted.shape()
-  #predicted_probs = [[index + 1, x[1]] for index, x in enumerate(rf.predict_proba(test))]
+def pca_explore(n_components=3):
+  #adapted from http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_iris.html
+  np.random.seed(5)
+  #centers = [[1, 1], [-1, -1], [1, -1]]
+  #iris = datasets.load_iris()
+  #X = iris.data
+  #y = iris.target
+  X, y = load_training_data()
+  X = np.asarray([x[1:-1] for x in X])
+  y = np.asarray(y)
 
-  #output = np.array(zip(indices, predicted), dtype=[('id', int), ('ct',int)])
-  #savetxt('fake_submission.csv', output, delimiter=',', fmt='%d,%d', 
-          #header='Id,Cover_Type', comments = '')
-  #return output
+  fig = plt.figure(1, figsize=(4, 3))
+  plt.clf()
+  ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+  plt.cla()
+
+  pca = decomposition.PCA(n_components)
+  pca.fit(X)
+  X = pca.transform(X)
+  return X, y
+  #return pca
+
+def plot_pca(X=None, y=None):
+  for name, label in [('type 1', 1), ('type 2', 2), ('type 3', 3)]:
+      ax.text3D(X[y == label, 0].mean(),
+                X[y == label, 1].mean() + 1.5,
+                X[y == label, 2].mean(), name,
+                horizontalalignment='center',
+                bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+  # Reorder the labels to have colors matching the cluster results
+  #y = np.choose(y, [1, 2, 3]).astype(np.int)
+  ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.spectral)
+
+  x_surf = [X[:, 0].min(), X[:, 0].max(),
+            X[:, 0].min(), X[:, 0].max()]
+  y_surf = [X[:, 0].max(), X[:, 0].max(),
+            X[:, 0].min(), X[:, 0].min()]
+  x_surf = np.array(x_surf)
+  y_surf = np.array(y_surf)
+  v0 = pca.transform(pca.components_[0])
+  v0 /= v0[-1]
+  v1 = pca.transform(pca.components_[1])
+  v1 /= v1[-1]
+
+  ax.w_xaxis.set_ticklabels([])
+  ax.w_yaxis.set_ticklabels([])
+  ax.w_zaxis.set_ticklabels([])
+
+  plt.show()
 
 if __name__=="__main__":
   run()
