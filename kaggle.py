c4e38ea21ed1bbf770b7d892b4c8388be1d8ccf5
Jonathan Goff <jrgoff@cascadetracking.org>
2014-08-06 01:42:38 -0700

    Clean up code a bit.
    Make it so can feed training and test sets from PCA into RF

--- kaggle.py
+++ kaggle.py
@@ -12,67 +12,82 @@ from sklearn import decomposition
 def load_training_data():
   #loads training data returns as data, target
   dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
-  data = np.asarray([x[1:-1] for x in dataset])
+  data = np.asarray([x[0:-1] for x in dataset]) #remove target
   target = np.asarray([x[-1] for x in dataset])
   return data, target
 
+def load_test_data():
+  test = genfromtxt(open('test.csv','r'), delimiter=',', dtype='f8')[1:]
+  return test
 
 def rf_cross_validate(n=100, X=None, y=None):
   #Cross validation form from http://scikit-learn.org/stable/modules/cross_validation.html
   #Random Forest adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
   if X==None or y == None:
     X, y = load_training_data()
+    X = [x[1:] for x in X] #remove id
   #dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
   
-  #data = [x[1:-1] for x in dataset]
-  #target = [x[-1] for x in dataset]
+
   train_x, test_x, train_y, test_y =  cross_validation.train_test_split(X, y, test_size = 0.3)
   rf = RandomForestClassifier(n)
   rf.fit(train_x, train_y)
   return rf.score(test_x, test_y) 
 
-def rf_test_data():
+def rf_test_data(n=100, X=None, y=None, test=None, output_file="submission.csv"):
   #Adjusted from https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience tutorial
-  dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
-  target = [x[-1] for x in dataset]
-  train = [x[1:-1] for x in dataset]
+  if X==None or y == None:
+    X, y = load_training_data()
+    X = [x[1:] for x in X] #remove id
+  #dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  #target = [x[-1] for x in dataset]
+  #train = [x[1:-1] for x in dataset]
   #print len(train[0])
   #print target
-  test_dataset = genfromtxt(open('test.csv','r'), delimiter=',', dtype='f8')[1:]
-  test = [x[1:] for x in test_dataset]
-  indices = [x[0] for x in test_dataset]
+  if test == None:
+    test = load_test_data()
+    
+  indices = [x[0] for x in test]
+  test = [x[1:] for x in test]
 
   rf = RandomForestClassifier(n_estimators=100)
-  rf.fit(train, target)
+  rf.fit(X, y)
   predicted = rf.predict(test) 
   output = np.array(zip(indices, predicted), dtype=[('id', int), ('ct',int)])
-  savetxt('submission.csv', output, delimiter=',', fmt='%d,%d', 
+  savetxt(output_file, output, delimiter=',', fmt='%d,%d', 
           header='Id,Cover_Type', comments = '')
   return output
 
-def pca_explore(n_components=3):
+def pca_explore(n_components=3, test=False):
   #adapted from http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_iris.html
+  #returns X, y if test=False
+  #  if test=True returns X, y, T (transformed training data, target, transformed test data)
   np.random.seed(5)
-  #centers = [[1, 1], [-1, -1], [1, -1]]
-  #iris = datasets.load_iris()
-  #X = iris.data
-  #y = iris.target
   X, y = load_training_data()
-  X = np.asarray([x[1:-1] for x in X])
+  X = np.asarray([x[1:] for x in X])
   y = np.asarray(y)
 
+  pca = decomposition.PCA(n_components)
+  pca.fit(X)
+  X_new = pca.transform(X)
+
+  if test:
+    #in this case returned y not of use
+    test = load_test_data()
+    indices = np.reshape(np.asarray([x[0] for x in test]), (len(test), 1))
+    T = pca.transform([x[1:] for x in test])
+    T = np.append(indices, T, 1)
+    return X_new, y, T
+  
+  return X_new, y
+  #return pca
+
+def plot_pca(X=None, y=None):
   fig = plt.figure(1, figsize=(4, 3))
   plt.clf()
   ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
   plt.cla()
 
-  pca = decomposition.PCA(n_components)
-  pca.fit(X)
-  X = pca.transform(X)
-  return X, y
-  #return pca
-
-def plot_pca(X=None, y=None):
   for name, label in [('type 1', 1), ('type 2', 2), ('type 3', 3)]:
       ax.text3D(X[y == label, 0].mean(),
                 X[y == label, 1].mean() + 1.5,
