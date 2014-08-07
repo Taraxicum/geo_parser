f4842909d16ce859fd626762d2281bc733513cbf
Jonathan Goff <jrgoff@cascadetracking.org>
2014-08-07 05:06:25 -0700

    Transform "Wilderness Area" and "Soil Type" to single fields
    Run tests using transformed data sets
    
    adjust test running code to be a bit less repetitive

--- kaggle.py
+++ kaggle.py
@@ -8,16 +8,57 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 from sklearn import decomposition
 
+def transform_data(test=False):
+  #Function used to create condensed training data.  Currently set to just return so as to not accidentally write to the file that has already been finished
+  return False
+  if test:
+    out_file = "test_condense_wild_soil.csv"
+    X = load_test_data()
+  else:
+    out_file = "train_condense_wild_soil.csv"
+    X, y = load_training_data()
+  #11 - 14 are wilderness area indicators
+  wilderness = []
+  soil = []
+  for r, x in enumerate(X):
+    soil.append(0) #should be filled by end but just in case there are records with no soil types defined.
+    if x[11] == 1:
+      wilderness.append(1)
+    elif x[12] == 1:
+      wilderness.append(2)
+    elif x[13] == 1:
+      wilderness.append(3)
+    elif x[14] == 1:
+      wilderness.append(4)
+    else:
+      wilderness.append(0)
+    for i in range(15,55):
+      if x[i] == 1:
+        soil[r] = i -14
+        break
+  with open(out_file, "a") as myfile:
+    for r, x in enumerate(X):
+      if test:
+        output = np.concatenate((x[0:11], [wilderness[r], soil[r]]))
+      else:
+        output = np.concatenate((x[0:11], [wilderness[r], soil[r], y[r]]))
+      myfile.write(",".join(str(int(o)) for o in output) + "\n")
 
-def load_training_data():
+def load_training_data(condensed=False):
   #loads training data returns as data, target
-  dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  if condensed:
+    dataset = genfromtxt(open('train_condense_wild_soil.csv', 'r'), delimiter=',', dtype='f8')[1:]
+  else:
+    dataset = genfromtxt(open('train.csv', 'r'), delimiter=',', dtype='f8')[1:]
   data = np.asarray([x[0:-1] for x in dataset]) #remove target
   target = np.asarray([x[-1] for x in dataset])
   return data, target
 
-def load_test_data():
-  test = genfromtxt(open('test.csv','r'), delimiter=',', dtype='f8')[1:]
+def load_test_data(condensed=False):
+  if condensed:
+    test = genfromtxt(open('test_condense_wild_soil.csv','r'), delimiter=',', dtype='f8')[1:]
+  else:
+    test = genfromtxt(open('test.csv','r'), delimiter=',', dtype='f8')[1:]
   return test
 
 def rf_cross_validate(n=100, X=None, y=None):
@@ -49,8 +90,7 @@ def rf_test_data(n=100, X=None, y=None, test=None, output_file="submission.csv")
     
   indices = [x[0] for x in test]
   test = [x[1:] for x in test]
-
-  rf = RandomForestClassifier(n_estimators=100)
+  rf = RandomForestClassifier(n_estimators=n)
   rf.fit(X, y)
   predicted = rf.predict(test) 
   output = np.array(zip(indices, predicted), dtype=[('id', int), ('ct',int)])
@@ -129,6 +169,30 @@ def pca_to_rf(n_components=5, n_trees=100, test=False):
     process = "PCA_{} -> RF_{}".format(n_components, n_trees)
 
   keep_track_of_results(result, speed, process, out_file)
+
+def condensed_rf(n_trees=100, test=False):
+  if test:
+    X, y = load_training_data(condensed=True)
+    X_new = [x[1:] for x in X]
+    T = load_test_data(condensed=True)
+    result = "Not yet known(see kaggle)"
+    out_file="condensed_submission_RF{}.csv".format(n_trees)
+    rf_test_data(n=n_trees, X=X_new, y=y, test=T, output_file=out_file)
+  else:
+    X, y = load_training_data(condensed=True)
+    result = rf_cross_validate(n=n_trees, X=X, y=y)
+    out_file = "NA"
+  process = "condense_wild_soil RF{}".format(n_trees)
+  return result, out_file, process
+
+def keep_track(func, args):
+  import time
+  start_time = time.time()
+  result, out_file, process = func(*args)
+  end_time = time.time()
+  speed = "{:.1f}".format(end_time - start_time)
+  keep_track_of_results(result, speed, process, out_file)
+
 
 def keep_track_of_results(result, speed, process, out_file):
   from time import strftime
