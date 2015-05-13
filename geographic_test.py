fcbd969e173597f5117dcead78ab53537412e12d
Jonathan Goff <olpabas@gmail.com>
2015-05-13 13:53:25 -0700

    Begin work on progressive process of building model.  Start with a cohort then build out progressively from there to ensure that cohorts overlap.
    This isn't currently performing very well.  It hasn't done a good job of getting broad cohort coverage.  From visual inspection of the results it seems to be sometimes fitting the data poorly (e.g. in one set, ended up with a bunch of points in close x, y proximity but different elevations which would suggest gathering many data points on a cliff which seems unlikely).  Another issue is that the fixed point tolerance may need to be adjusted as at the end of the process it sometimes ends up with a lot of fixed points fairly close to each other rather than treating them as the same point (which may or may not be correct - maybe there are multiple fire points in close proximity).
    
    Another issue that needs to be addressed is that the examine_results function needs to be adjusted to consider fixed points that aren't just the initial ones in the data structure.

--- geographic_test.py
+++ geographic_test.py
@@ -105,7 +105,8 @@ class GeoParser():
         self.cohort_threshold = 4  #cohorts smaller than this don't seem to consistently converge correctly
         self.n = len(data)
         self.center_ids = []
-        self.fixed_points = []
+        self.accumulated_cohorts = None
+        self.fixed_points = None
         self.points = []
         #self.good_cohorts = []
         #self.make_cohorts(radius)
@@ -116,26 +117,102 @@ class GeoParser():
     def init_fixed_points(self):
         self.df_fixed_points = pd.DataFrame(columns=['x', 'y', 'type'])
 
+    def chain_progressive_cohorts(self, n=2):
+        if n == 0:
+            return
+
+        if self.progressive_cohorts():
+            if self.accumulated_cohorts is None:
+                self.add_fixed_points(self.fixed_points)
+                self.accumulated_cohorts = pd.DataFrame(self.current_cohort)
+            else:
+                self.match_cohort()
+                #self.accumulated_cohorts = self.accumulated_cohorts.append(self.current_cohort, ignore_index = True)
+            print "Accumulated {} points".format(len(self.accumulated_cohorts))
+        else:
+            print "Trouble completing progress_cohorts() with {} iterations left".format(n -1)
+            return
+        self.chain_progressive_cohorts(n - 1)
+    
+    def set_magnitudes(self):
+        """
+            Set accumulated_cohorts['magnitude'] field to the
+            point distance from the mean.
+            The goal here is to make it easier to find points towards
+            the edge of those already incorporated so we can use them
+            for centers of new cohorts to add in
+        """
+        mean_x = self.accumulated_cohorts.loc[self.data.Id.isin(self.center_ids)].x.mean(axis=0)
+        mean_y = self.accumulated_cohorts.loc[self.data.Id.isin(self.center_ids)].y.mean(axis=0)
+        self.accumulated_cohorts['magnitude'] = np.sqrt(
+                (self.accumulated_cohorts.x - mean_x)**2 + 
+                (self.accumulated_cohorts.y - mean_y)**2)
+
+    def pick_center_points(self, n=10):
+        """
+            pick list of options for center points.
+            if possible returns Series the Ids of n points to try as new cohort center
+        """
+        if self.accumulated_cohorts is None:
+            return self.data.iloc[np.random.randint(len(self.data), size=n)].Id
+        else:
+            self.set_magnitudes()
+            options = self.accumulated_cohorts.sort('magnitude', ascending=False).Id.head(n)
+            return options
+    
+    def check_good_cohort(self, indices):
+        """
+            Have to be a minimum number of indices to bother making a cohort
+            If so, fit the cohort and if that is successful, make sure the
+            resulting cohort actually adds at least one point to the accumulated cohorts
+        """
+        if len(indices) > self.cohort_threshold:
+            self.current_cohort = self.data.loc[indices]
+            if self.accumulated_cohorts is None:
+                amount_new = len(self.current_cohort)
+            else:
+                amount_new = len(set(self.current_cohort.Id) - set(self.accumulated_cohorts.Id))
+            if (amount_new > 0) and self.fit_current_cohort():
+                if self.accumulated_cohorts is None:
+                    return True
+                overlap = len(set(self.current_cohort.Id) - set(self.accumulated_cohorts.Id))
+                print "New cohort adds {} points".format(overlap)
+                if overlap > 0:
+                    return True
+        return False
+
     def progressive_cohorts(self):
-        self.points = []
-        self.fixed_points = []
         indices = []
         attempt_count = 0
-        while len(indices) < self.cohort_threshold and attempt_count < 10:
-            point = self.data.iloc[np.random.randint(len(self.data))].Id
+        options = self.pick_center_points(10)
+        for point in options:
+            if self.check_good_cohort(indices):
+                self.center_ids.append(point)
+                return True
+            #point = self.data.iloc[np.random.randint(len(self.data))].Id
             indices = self.find_cohort(point)
-            print "Found {} points for cohort in attempt {}".format(len(indices), attempt_count + 1)
+            print "Found {} points for cohort - center {} - in attempt {}".format(len(indices), point, attempt_count + 1)
             attempt_count += 1
-        if len(indices) < self.cohort_threshold:
-            print "Attempt to find initial cohort failed after too many attempts"
+        if self.check_good_cohort(indices):
+            self.center_ids.append(point)
+            return True
+        else:
+            print "Attempt to find new cohort failed after {} attempts".format(len(options))
             return False
-        self.center_ids.append(point)
-        self.current_cohort = self.data.loc[indices]
-        return True
+     
+    def init_points(self, cohort):
+            #Initializes set of hypothesized points and fixed points for iterating 
+            #cohort:  DataFrame containing the true distances of the points to the 
+            points = pd.DataFrame(100*np.random.randn(len(cohort), 2), columns=['x', 'y'])
+            vals = 1000*np.random.randn(6)
+            fixed_points = pd.DataFrame(vals.reshape((3,2)), columns=['x', 'y'])
+            fixed_points['type'] = ['fire', 'water', 'road']
+            return points, fixed_points
     
     def fit_current_cohort(self):
         if len(self.current_cohort) < self.cohort_threshold:
             print "FAILED to fit cohort, size got too small with reductions"
+            return False
         p, fp = self.init_points(self.current_cohort)
         start_time = time.clock()
         p, fp, cost = self.bgfs_automate()
@@ -144,73 +221,33 @@ class GeoParser():
         if cost <= self.cost_threshold:
             print "SUCCESS at fitting cohort of length {} cost {}, time {}".format(len(self.current_cohort), cost, total_time)
             self.points.append(p)
-            self.add_fixed_points(fp)
+            self.fixed_points = fp
             self.current_cohort['x'] = p.x.values
             self.current_cohort['y'] = p.y.values
-            return self.examine_results(self.current_cohort, p, fp)
+            #return self.examine_results(self.current_cohort, p, fp)
+            return True
         else:
             print "FAILED to find good fit for cohort of length {} cost {}, time {}".format(len(self.current_cohort), cost, total_time)
             results = self.examine_results(self.current_cohort, p, fp)
             self.remove_problem_points(results)
-            self.fit_current_cohort()
-
+            return self.fit_current_cohort()
 
     def remove_problem_points(self, results, percentile=.75):
         cutoff = results.total.quantile(percentile)
-        keep_indices = results.loc[results.total < cutoff].index
-        self.current_cohort = self.current_cohort.iloc[keep_indices]
+        keep_indices = np.append(results.loc[results.total < cutoff].Id, self.data.loc[self.data.Id.isin(self.center_ids)].Id)
+        self.current_cohort = self.current_cohort.loc[self.current_cohort.Id.isin(keep_indices)]
 
-    def iterate_cohorts(self):
-        self.points = []
-        self.fixed_points = []
-        self.good_cohorts = []
-        #FIXME df_fixed_points initialized in cohort matching so below should be removed
-        #self.init_fixed_points()
-        for idx, c in enumerate(self.cohorts):
-            start_time = time.clock()
-            self.current_cohort = c
-            #p, fp, costs = self.automated_iteration(c, 1000, 1.5, False)
-            print "starting cohort of length {}".format(len(c)) 
-            p, fp, cost = self.bgfs_automate()
-            #if costs[-1] <= .5:
-            end_time = time.clock()
-            total_time = end_time - start_time
-            if cost <= self.cost_threshold:
-                print "SUCCESS at fitting cohort of length {} cost {}, time {}".format(len(c), cost, total_time)
-                self.good_cohorts.append(idx)
-                self.points.append(p)
-                self.fixed_points.append(fp)
-                c['x'] = p.x.values
-                c['y'] = p.y.values
-            else:
-                self.fixed_points.append(None)
-                print "FAILED to find good fit for cohort of length {} cost {}, time {}".format(len(c), cost, total_time)
-                #print "Failed first try, try again with smaller alpha"
-                #p, fp, costs = self.automated_iteration(c, 1000, .3, False)
-                #if costs[-1] <= .5:
-                    #self.points.append(p)
-                    #self.fixed_points.append(fp)
-                #else:
-                    #print "STILL FAILED TO FIND GOOD SOLUTION FOR COHORT :("
-
-    def init_points(self, cohort):
-        #Initializes set of hypothesized points and fixed points for iterating our gradient descent algorithm
-        #cohort:  DataFrame containing the true distances of the points to the fixed points
-        points = pd.DataFrame(100*np.random.randn(len(cohort), 2), columns=['x', 'y'])
-        vals = 1000*np.random.randn(6)
-        fixed_points = pd.DataFrame(vals.reshape((3,2)), columns=['x', 'y'])
-        fixed_points['type'] = ['fire', 'water', 'road']
-        return points, fixed_points
-    
     def find_cohort(self, pid): 
         """
             pid: value of Id field of center point of cohort
         """
+        water_v_threshold = 5
         ds = pd.DataFrame(np.zeros((self.n, 1)), columns=['distance'])
         locus = self.data.loc[self.data.Id == pid]
         #First filter by if water source could be same - i.e. Vertical_Distance_To_Hydrology - Elevation is same (close to same?)
         water_elev = (locus.Vertical_Distance_To_Hydrology - locus.Elevation).values[0]
-        filter_indices = self.data.loc[(self.data.Vertical_Distance_To_Hydrology - self.data.Elevation == water_elev)].index
+        filter_indices = self.data.loc[
+                (self.data.Vertical_Distance_To_Hydrology - self.data.Elevation) - water_elev < water_v_threshold].index
         fds = ds.loc[filter_indices]
         fdata = self.data.loc[filter_indices]
         fds.distance += (locus.Horizontal_Distance_To_Fire_Points.values[0] - fdata.Horizontal_Distance_To_Fire_Points)**2
@@ -219,23 +256,6 @@ class GeoParser():
         fds.distance = np.sqrt(fds.distance)
         return fds.loc[fds.distance < self.radius].index
 
-    #def make_cohorts(self, radius):
-    #    #From testing, it appears that having cohorts of at least size 4 is desirable, I did have one example
-    #    #  at size 4 that failed to converge nicely, but several other examples worked quite well.
-    #    #Size 3 had some tests that worked great and some that did converged to a non-true solution
-    #    self.cohorts = []
-    #    indexes = []
-    #    remaining = self.data.index.values
-    #    while len(remaining) > 0:
-    #        p = remaining[np.random.randint(len(remaining))]
-    #        indexes.append(self.find_cohort(p, radius))
-    #        remaining = list(set(remaining) - set(indexes[-1]))
-    #    print len(indexes)
-    #    for c in indexes:
-    #        if len(c) > self.cohort_threshold:
-    #            self.cohorts.append(self.data.loc[c])
-    #            print "Cohort {}, length {}".format(len(self.cohorts) - 1, len(c))
-    
     def add_fixed_points(self, fp_set):
         #fp_set should be set of fire, water, road fixed points
         fixed_point_difference_threshold = 10 #if fixed points of same type are within difference threshold of
@@ -253,24 +273,19 @@ class GeoParser():
                     ['x', 'y', 'type']] = [fp_set.loc[i]['x'], fp_set.loc[i]['y'], fp_set.loc[i]['type']]
 
 
-    def automate_cohort_matching(self):
-        #cohorts should have at least 3 overlapping points to be matched up
+    def match_cohort(self):
+        #cohort should have at least 3 overlapping points to be matched up
         #theoretically the overlap could include the fixed points, but in practice that might be a little
         #tricky since additional checking would need to be done to ensure the fixed points were the same
-        self.init_fixed_points()
-        joined_points = self.cohorts[self.good_cohorts[0]]
-        remaining_cohort_indices = self.good_cohorts[1:]
-        self.add_fixed_points(self.fixed_points[self.good_cohorts[0]])
-        last_pass_count = len(remaining_cohort_indices) + 1
-        while len(remaining_cohort_indices) > 0 and len(remaining_cohort_indices) < last_pass_count:
-            last_pass_count = len(remaining_cohort_indices)
-            for i, idx in enumerate(remaining_cohort_indices):
-                if len(joined_points.index & self.cohorts[idx].index) >= 3 and self.fixed_points[idx] is not None:
-                    self.align_cohorts(joined_points, self.cohorts[idx], self.fixed_points[idx])
-                    joined_points = pd.concat([joined_points, self.cohorts[idx].loc[set(self.cohorts[idx].index) - set(joined_points.index)]])
-                    self.add_fixed_points(self.fixed_points[idx])
-                    remaining_cohort_indices.pop(i)
-        return joined_points
+        joined_points = self.accumulated_cohorts
+        if len(set(joined_points.index) & set(self.current_cohort.index)) >= 3:
+            self.align_cohorts(joined_points, self.current_cohort, self.fixed_points)
+            joined_points = pd.concat([joined_points, 
+                self.current_cohort.loc[set(self.current_cohort.index) - set(joined_points.index)]])
+            self.add_fixed_points(self.fixed_points)
+            self.accumulated_cohorts = joined_points
+        else:
+            print "Not enough overlap between accumulated cohorts and current cohort?!" #Should not happen
 
     def bgfs_automate(self, p=None, fp=None):
         if (p is None) or (fp is None):
